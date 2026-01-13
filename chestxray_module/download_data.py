819a81049dae6bb2f9a98347b5715b0ddad54c1e
Ramy Gadalla <gadallaramy@gmail.com>
2026-01-13 18:09:07 -0500

    download the kaggle dataset

---
+++ chestxray_module/download_data.py
@@ -0,0 +1,18 @@
+
+import os
+import sys
+
+cmd = "kaggle datasets download -d muhammadrehan00/chest-xray-dataset -p data/raw"
+exit_code = os.system(cmd)
+
+if exit_code != 0:
+    sys.exit(
+        "ERROR: Kaggle command failed.\n"
+        "Please make sure:\n"
+        "- The correct environment is activated\n"
+        "- Kaggle is installed (pip install kaggle)\n"
+        "- Kaggle authentication is set (KAGGLE_API_TOKEN)"
+    )
+    
+if os.system("unzip -o data/raw/*.zip -d data/interim") != 0:
+    sys.exit("ERROR: Unzip failed.")
