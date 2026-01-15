97ac14f70b144c0cfd3afed89d6394e5a081dd9b
Ramy Gadalla <gadallaramy@gmail.com>
2026-01-15 15:31:16 -0500

    doc string added for readability

--- chestxray_module/modeling/export_onnx.py
+++ chestxray_module/modeling/export_onnx.py
@@ -1,21 +1,27 @@
+
+"""
+Purpose:
+    Export model in ONNX format for framework-agnostic downstream application.
+    
+Note:
+    Pytoch, ONNX and python3.10+ comptability is not stable.
+    Recommended to run this script:
+    
+        conda create -n onnx_export python=3.10 -y &&
+        conda activate onnx_export &&
+        conda install -c conda-forge pytorch torchvision pandas onnx onnxscript onnxruntime monai -y &&
+        conda install -c conda-forge opencv -y
+    
+    Run script from this newly created env
+
+
+"""
+
 import torch
 from pathlib import Path
 from chestxray_module.modeling.predict import build_model  
 
-
-
-"""
-Pytoch ONNX python3.10+ comptability is not stable.
-Recommended to run this script:
-`conda create -n onnx_export python=3.10 -y &&
- conda activate onnx_export &&
- conda install -c conda-forge pytorch torchvision pandas onnx onnxscript onnxruntime monai -y &&
- conda install -c conda-forge opencv -y
-`
-Run script from this newly created env
-"""
-
-
+# Varaibles config
 CHECKPOINT_PATH = "models/best_model.pt"
 ONNX_PATH = "models/best_model.onnx"
 NUM_CLASSES = 3
